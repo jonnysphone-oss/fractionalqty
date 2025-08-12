c8fbb19a6d8f684423139fcb3eefc3c2ee90a4ef
jonnysphone-oss <jonnysphone@gmail.com>
2025-08-12 09:36:38 +0100

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
   if (!shop) return res.status(400).send('Missing ?shop=your-shop.myshopify.com or set SHOP env')
   const state = Math.random().toString(36).substring(2)
   const scope = process.env.SCOPES || 'write_products,write_orders'
-  const redirect = \`\${process.env.HOST}/api/callback\`
-  const installUrl = \`https://\${shop}/admin/oauth/authorize?\${querystring.stringify({ client_id: process.env.SHOPIFY_API_KEY, scope, state, redirect_uri: redirect })}\`
+  const redirect = `${process.env.HOST}/api/callback`
+  const installUrl = `https://${shop}/admin/oauth/authorize?${querystring.stringify({ client_id: process.env.SHOPIFY_API_KEY, scope, state, redirect_uri: redirect })}`
   res.redirect(installUrl)
 }
