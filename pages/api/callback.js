f0b1dcff40759aa3334f8c6ad744971b28465927
jonnysphone-oss <jonnysphone@gmail.com>
2025-08-12 09:31:49 +0100

    Update callback.js

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -4,7 +4,7 @@ import { saveAccessToken } from '../../lib/shopify.js'
 export default async function handler(req, res) {
   const { code, shop } = req.query
   if (!code || !shop) return res.status(400).send('Missing code or shop')
-  const tokenUrl = \`https://\${shop}/admin/oauth/access_token\`
+  const tokenUrl = `https://${shop}/admin/oauth/access_token`
   const resp = await fetch(tokenUrl, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ client_id: process.env.SHOPIFY_API_KEY, client_secret: process.env.SHOPIFY_API_SECRET, code }) })
   const data = await resp.json()
   const accessToken = data.access_token
